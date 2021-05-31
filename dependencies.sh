974110901634125809a7de00237b0523449be2cb
Carlos Aznarán Laos <caznaranl@uni.pe>
2021-05-31 14:26:57 -0500

    Resize images

--- dependencies.sh
+++ dependencies.sh
@@ -2,11 +2,15 @@
 
 set -u -x
 
+echo 'Installing dependencies'
 sudo -u aur yay -Syu texlive-latexextra texlive-pictures texlive-science texlive-fontsextra texlive-bibtexextra biber inkscape doxygen python-sphinx ttf-firacode --needed --noconfirm
 
 # http://fpliu-blog.chinacloudsites.cn/it/software/man
+echo 'Installing manpages'
 sudo -u aur yay -Syu tldr man-pages man-pages-es --needed --noconfirm
 #manpages-es manpages-es-extra /usr/bin/vendor_perl
+
+echo 'Installing gmsh API'
 # sudo -u aur yay gmsh python-wheel --noconfirm
 # python -m venv $HOME/gmsh-env
 # source $HOME/gmsh-env/bin/activate
