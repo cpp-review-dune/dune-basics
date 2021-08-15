360d61c7f76b5535d843cb63b331c636ab269d2b
Carlos Aznarán Laos <caznaranl@uni.pe>
2021-08-15 12:11:31 -0500

    Update

--- dependencies.sh
+++ dependencies.sh
@@ -1,23 +1,13 @@
 #!/usr/bin/bash
 
-set -u -x
-
 echo 'Installing dependencies'
-yay -Syu texlive-latexextra texlive-pictures texlive-science texlive-fontsextra texlive-bibtexextra biber inkscape doxygen python-sphinx ttf-fira-code fmt --needed --noconfirm
+yay -Syuu texlive-latexextra texlive-pictures texlive-science texlive-fontsextra texlive-bibtexextra biber inkscape doxygen python-sphinx ttf-fira-code fmt --needed --noconfirm
 echo 'export PATH="/usr/bin/vendor_perl:$PATH"' >>~/.bashrc
 echo 'alias man="man -Les"' >>~/.bashrc
 # http://fpliu-blog.chinacloudsites.cn/it/software/man
 echo 'Installing manpages'
-yay -Syu tldr man-pages man-pages-es --needed --noconfirm
-
-echo 'Installing gmsh API'
-# sudo -u aur yay gmsh python-wheel --noconfirm
+yay -S tldr man-pages man-pages-es --needed --noconfirm
 # python -m venv $HOME/gmsh-env
 # source $HOME/gmsh-env/bin/activate
-# pip install gmsh
-
-echo 'Installing manpages'
-
-yay -Syu boost --needed --noconfirm
 echo 'Actualizando perfiles'
 source ~/.bashrc
