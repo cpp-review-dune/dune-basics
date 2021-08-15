cc2c36557fb25f780264750548085143c1da1cdc
Carlos Aznarán Laos <caznaranl@uni.pe>
2021-08-15 10:40:15 -0500

    Add structure.tex

--- dependencies.sh
+++ dependencies.sh
@@ -3,13 +3,12 @@
 set -u -x
 
 echo 'Installing dependencies'
-sudo -u aur yay -Syu texlive-latexextra texlive-pictures texlive-science texlive-fontsextra texlive-bibtexextra biber inkscape doxygen python-sphinx ttf-fira-code fmt --needed --noconfirm
-sudo -u aur echo 'export PATH="/usr/bin/vendor_perl:$PATH"' >> /home/gitpod/.bashrc
-sudo -u aur echo 'alias man="man -Les"' >> /home/gitpod/.bashrc
+yay -Syu texlive-latexextra texlive-pictures texlive-science texlive-fontsextra texlive-bibtexextra biber inkscape doxygen python-sphinx ttf-fira-code fmt --needed --noconfirm
+echo 'export PATH="/usr/bin/vendor_perl:$PATH"' >>~/.bashrc
+echo 'alias man="man -Les"' >>~/.bashrc
 # http://fpliu-blog.chinacloudsites.cn/it/software/man
 echo 'Installing manpages'
-sudo -u aur yay -Syu tldr man-pages man-pages-es --needed --noconfirm
-#manpages-es manpages-es-extra
+yay -Syu tldr man-pages man-pages-es --needed --noconfirm
 
 echo 'Installing gmsh API'
 # sudo -u aur yay gmsh python-wheel --noconfirm
@@ -19,6 +18,6 @@ echo 'Installing gmsh API'
 
 echo 'Installing manpages'
 
-sudo -u aur yay -Syu boost --needed --noconfirm
+yay -Syu boost --needed --noconfirm
 echo 'Actualizando perfiles'
 source ~/.bashrc
